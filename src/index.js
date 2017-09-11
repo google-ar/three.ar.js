ec6f87c8912ca0856cf921c5f2f5969269246688
Iker Jamardo <ijamardo@ijamardo-macbookpro.roam.corp.google.com>
2017-09-11 06:19:29 -0700

    Added the speech recognition enabling script.
    Added the ARSpeechRecognition file import in the index.js file.

--- src/index.js
+++ src/index.js
@@ -19,3 +19,4 @@ import ARPerspectiveCamera from './ARPerspectiveCamera';
 import ARReticle from './ARReticle';
 import ARUtils from './ARUtils';
 import ARView from './ARView';
+import './ARSpeechRecognition';
