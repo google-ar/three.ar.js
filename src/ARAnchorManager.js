46da4cc4f7ad67cb5fc1682420c934da4d670ae3
Iker Jamardo <ijamardo@gmail.com>
2017-11-28 15:30:14 -0800

    Fixed the lint errors and rebased to master.

--- src/ARAnchorManager.js
+++ src/ARAnchorManager.js
@@ -7,19 +7,22 @@ import { Object3D, Vector3, Matrix4, EventDispatcher } from 'three';
  * AnchorManager emits events pertaining to anchors.
  */
 export default class ARAnchorManager extends EventDispatcher {
+  /**
+  * @param {VRDisplay} vrDisplay 
+  */
   constructor(vrDisplay) {
     super();
-    if (!(vrDisplay instanceof VRDisplay) || !vrDisplay.getAnchors) {
-      throw new Error("invalid vr display parameter");
+    if (!(vrDisplay instanceof window.VRDisplay) || !vrDisplay.getAnchors) {
+      throw new Error('invalid vr display parameter');
     }
     this.vrDisplay_ = vrDisplay;
     this.anchorsToObject3Ds_ = new Map();
     this.object3DsToAnchors_ = new Map();
-    this.vrDisplay_.addEventListener('anchorsupdated', 
+    this.vrDisplay_.addEventListener('anchorsupdated',
         event => this.onAnchorsUpdated_(event));
     // This attribute is used to get the scale value from the anchor model
     // matrix but not use it. It is just to avoid garbage collection.
-    this.scale_  = new Vector3();
+    this.scale_ = new Vector3();
     // This attribute is used to calculate the matrix to be passed to the
     // anchor creation code only using the position and quaternion from the
     // object3d.
@@ -28,19 +31,19 @@ export default class ARAnchorManager extends EventDispatcher {
 
   /**
    * Assign a new anchor to an THREE.Object3D.
-   * @param object3d {THREE.Object3D} A Three Object3D instance to create and
+   * @param {THREE.Object3D} object3d A Three Object3D instance to create and
    * manage an anchor for. The current transformation (position/orientation) of
    * the Object3D will be used to create the anchor and this class will keep
    * it up to date with any tracking changes detected by the system.
    * IMPORTANT: The passed Object3D instance should be in world space!
    * @throws Error if:
    * - The given object3d is not an instance of Object3D.
-   * 
+   * @return {ARAnchorManager}
    */
   add(object3d) {
     if (!(object3d instanceof Object3D)) {
       throw new Error('Invalid Object3D trying to add an anchor');
-    };
+    }
     // Do not allow to create an anchor for an object3d that already has one.
     if (this.object3DsToAnchors_.has(object3d)) {
       return this;
@@ -68,7 +71,7 @@ export default class ARAnchorManager extends EventDispatcher {
 
   /**
    * Delete anchor associated to the THREE.Object3D.
-   * @param object3d {THREE.Object3D} The ThreeJS Object3D that has an anchor
+   * @param {THREE.Object3D} object3d The ThreeJS Object3D that has an anchor
    * associated to it and that should be removed.
    * @return {boolean} A flag indicating if the removal of the anchor was
    * successful (true) or not (false, in case there is no anchor for the given
@@ -77,11 +80,11 @@ export default class ARAnchorManager extends EventDispatcher {
   remove(object3d) {
     if (!(object3d instanceof Object3D)) {
       throw new Error('Invalid Object3D trying to remove anchor');
-    };
+    }
     const anchor = this.object3DsToAnchors_.get(object3d);
-    if (!anchor) { 
+    if (!anchor) {
       return false;
-    };
+    }
     this.anchorsToObject3Ds_.delete(anchor);
     this.object3DsToAnchors_.delete(object3d);
     this.vrDisplay_.removeAnchor(anchor);
@@ -92,18 +95,20 @@ export default class ARAnchorManager extends EventDispatcher {
    * Called when any anchors are updated. The updated modelMatrix is
    * set for the THREE.Object3D associated with each updated anchor.
    * An 'object3dsupdated' event is emitted with the updated THREE.Object3Ds.
+   * @param {VRAnchorEvent} event The anchor event that contains the list of
+   * modified anchors.
    */
   onAnchorsUpdated_(event) {
     const updatedObject3Ds = [];
     for (let anchor of event.anchors) {
       const object3d = this.anchorsToObject3Ds_.get(anchor);
-      if (!object3d) { 
+      if (!object3d) {
         // It could happen that there are more than one AnchorManager and
         // that create Anchors that reside in the native side and are not
         // handled by all of the AnchorManagers, so let's just continue to
         // see if the updated anchors are managed by this AnchorManager.
         continue;
-      };
+      }
       // get the anchor's updated model matrix
       object3d.matrix.fromArray(anchor.modelMatrix);
       // decompose the updated matrix into position, orientation, and scale
